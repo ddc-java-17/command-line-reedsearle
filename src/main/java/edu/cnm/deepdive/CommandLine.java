a74095e791474c6386a1af38c20349adde75e27b
Reed Searle <csearle@cnm.edu>
2024-01-22 11:55:37 -0700

    Change code for the sake of a commit

--- src/main/java/edu/cnm/deepdive/CommandLine.java
+++ src/main/java/edu/cnm/deepdive/CommandLine.java
@@ -5,7 +5,7 @@ public class CommandLine {
   public static void main(String[] args) {
 //    System.out.println(args.length);
     System.out.printf("%d\n",args.length);
-    for (int i=0; i<args.length; i++) {
+    for (int i=0; i<args.length; i+=1) {
 //      System.out.println(args[i]);
       System.out.printf("%s ",args[i]);
     }
