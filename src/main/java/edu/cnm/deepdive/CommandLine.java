2c5776c48000700dd261d72dbadab5a12862fec7
Reed Searle <csearle@cnm.edu>
2024-01-22 11:37:18 -0700

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/CommandLine.java
@@ -0,0 +1,15 @@
+package edu.cnm.deepdive;
+
+public class CommandLine {
+
+  public static void main(String[] args) {
+//    System.out.println(args.length);
+    System.out.printf("%d\n",args.length);
+    for (int i=0; i<args.length; i++) {
+//      System.out.println(args[i]);
+      System.out.printf("%s ",args[i]);
+    }
+    System.out.printf("\n");
+  }
+
+}
