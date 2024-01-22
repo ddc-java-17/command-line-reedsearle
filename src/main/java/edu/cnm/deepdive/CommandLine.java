9d0fff0f7911d358cb52d217c1890e6389c8488b
Reed Searle <csearle@cnm.edu>
2024-01-22 11:59:28 -0700

    "Enhanced for" statement

--- src/main/java/edu/cnm/deepdive/CommandLine.java
+++ src/main/java/edu/cnm/deepdive/CommandLine.java
@@ -5,9 +5,9 @@ public class CommandLine {
   public static void main(String[] args) {
 //    System.out.println(args.length);
     System.out.printf("%d\n",args.length);
-    for (int i=0; i<args.length; i+=1) {
+    for (String arg: args) {
 //      System.out.println(args[i]);
-      System.out.printf("%s ",args[i]);
+      System.out.printf("%s ",arg);
     }
     System.out.printf("\n");
   }
